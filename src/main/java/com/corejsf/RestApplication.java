3ed1f7231772f0016751bfc77b869ac1921c5102
Lucas Liu <lucasqhliu@gmail.com>
2025-11-30 14:44:53 -0800

    feat:crated Restapplication

---
+++ src/main/java/com/corejsf/RestApplication.java
@@ -0,0 +1,10 @@
+package com.corejsf;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
